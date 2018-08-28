f3103f5847c91ea84907a225367373e5344a75b2
Nick Essing <nick.essing@tobit.software>
2018-08-28 09:20:53 +0200

    deleteInsert

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
-/* eslint-disable no-console */
-import Insert from './components/insert'
+import Formular from './components/formular_container/Formular';
+import tempList1 from './components/tempList-container/tempList';
+import ListContainer from './components/formular_container/ListContainer';
+import Searchbar from './components/formular_container/Searchbar';
 
 const init = async () => {
     await chayns.ready;
-    new Insert();
+    this.tempList = new tempList1();
+    new ListContainer('#list',this);
+    new Searchbar('#searchbar',this);
+    new Formular('#formular'); 
+    this.tempList.refreshList("https://chayns1.tobit.com/TappApi/Site/SlitteApp?SearchString="+ "love" +"&Skip="+ "0" +"&Take="+ "10");
 };
 
 init();
+
+/*
+    onSearchBtnChange = (SearchString) => {
+        this.tempList.refreshList("https://chayns1.tobit.com/TappApi/Site/SlitteApp?SearchString="+ SearchString +"&Skip="+ "0" +"&Take="+ "10");
+    }
+    onMoreBtnChange = (SearchString) => {
+        this.tempList.appendList(SearchString);
+    }
+*/
